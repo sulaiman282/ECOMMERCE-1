63eabf73f1c1751bdab54fef75c6eb5857c2440a
Your Name <you@example.com>
2022-06-26 07:11:36 +0600

    navbar 50%

---
+++ src/components/Topheader.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import topheader from '../css/topheader.css'
+
+
+
+
+function Topheader() {
+  return (
+    <div id='topheader1'>
+      <div className='' id="topheader2">
+        <a href="url" className='pe-3'><i class="fa-solid fa-truck-fast pe-1 text-danger"></i>track my order</a>
+
+
+        <a href="url" className='pe-3'><i class="fa-solid fa-dollar-sign pe-1 text-danger"></i>sell with us</a>
+
+
+        <a href="url" className='pe-3'><i class="fab fa-affiliatetheme pe-1 text-danger"></i>affiliate program</a>
+
+
+        <a href="url" className='pe-3'><i class="fa-solid fa-location-dot pe-1 text-danger"></i>store location</a>
+
+
+        <a href="url" className='pe-3'><i class="fa-solid fa-headset pe-1 text-danger"></i>01856077645 (9am-5pm)</a>
+
+        <a href="url" className='pe-3 text-lowercase'> <i class="fa-solid fa-envelope pe-1 text-danger"></i>spidy.twitch.tv@gmail.com</a>
+
+      </div>
+      
+    
+
+
+
+    </div>
+  
+  )
+}
+
+export default Topheader;
